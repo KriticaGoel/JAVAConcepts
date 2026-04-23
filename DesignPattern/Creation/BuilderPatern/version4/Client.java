2a2cf8d6535b8abb6749c34223e48bb1d1bb36a2
kritica <kriticaagarwal@gmail.com>
2026-04-23 13:52:45 +0530

    Mememto code. Sol1 : Basic  sol2: Problem of basic and sol3 memento design

--- DesignPattern/Creation/BuilderPatern/version4/Client.java
+++ DesignPattern/Creation/BuilderPatern/version4/Client.java
@@ -10,7 +10,7 @@ public class Client {
 //        studentBuilder.setGender("F");
 //        studentBuilder.setName("Test");
 //        studentBuilder.setPhoneNumber(96507359);
-////client will not call constructor it will called inside build method
+//   client will not call constructor it will called inside build method
 //        // Student s=new Student(studentBuilder);
 //        Student s=studentBuilder.build();
         student.getEnroll();
