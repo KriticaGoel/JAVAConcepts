c518c1ea593da3a923f6560cd53982da75078b20
kritica <kriticaagarwal@gmail.com>
2026-04-23 23:56:36 +0530

    GenericStaticMethod

--- CoreJava/Generics/GenericClassMethod/GenericMethod.java
+++ CoreJava/Generics/GenericClassMethod/GenericMethod.java
@@ -2,7 +2,8 @@ package CoreJava.Generics.GenericClassMethod;
 
 public class GenericMethod {
 
-    public <K, V> void display(K key, V value) {
+    public <K, V, T> T display(K key, V value) {
         System.out.println("Key: " + key + ", Value: " + value);
+        return null; // Just for demonstration, you can return any type as needed
     }
 }
--- CoreJava/Generics/GenericClassMethod/Main.java
+++ CoreJava/Generics/GenericClassMethod/Main.java
@@ -7,5 +7,16 @@ public class Main {
         gm.display(123, "ABC");
         gm.display("Hello", 456);
 
+        dosomething("Kritica");
+        dosomething(100);
+
+
+
+    }
+
+    //Static method in generics
+    public static <T> T dosomething(T input) {
+        System.out.printf("The value of input is: %s %n", input);
+        return input;
     }
 }
